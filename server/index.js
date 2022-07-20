99ea0ae43afa6ad289220082c15b56fff3668bcb
bryce octovio ostrem <dragonfiresteel@gmail.com>
2022-07-20 13:14:30 -0500

    two

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ const path = require('path')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, '.../index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-const port = 4005
+const port = process.env.PORT
+|| 4005
 
 app.listen(port, ()=> {
     console.log(`server is running ${port}`)
