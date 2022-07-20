2cd841907bc8403945706f75a23fd44bdaeb8747
bryce octovio ostrem <dragonfiresteel@gmail.com>
2022-07-20 13:38:52 -0500

    fghj

---
+++ main.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('button')
+
+const buttonAlert = () =>{
+    alert('button worked')
+}
+button.addEventListener('click, buttonAlert')
--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ app.get('/',(req,res)=>{
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'))
 })
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../main.js'))
+})
 const port = process.env.PORT
 || 4005
 
