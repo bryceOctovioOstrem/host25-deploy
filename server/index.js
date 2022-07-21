5a4701a512527e5529c909a0a0ef194508386564
bryce octovio ostrem <dragonfiresteel@gmail.com>
2022-07-21 15:35:18 -0600

    dfgh

--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
 const app = express()
+
+app.use(express.json())
+app.use(cors());
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '89d90b1aa3c94d30bdc3637c333eb26d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, '../index.html'))
-})
 
+})
+try{
+app.get('/bob',(req, res)=>{
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+} catch(error){
+    rollbar.info('broken callback is broken (:')
+}
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'))
 })
